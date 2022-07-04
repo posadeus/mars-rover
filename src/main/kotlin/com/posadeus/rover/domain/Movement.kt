579baaadd7a181fdf9cc28c0999f1204fa2d3812
mberti <marco.berti90@gmail.com>
2022-07-04 14:53:46 +0200

    Refactor: Movement can manage forward moves

--- src/main/kotlin/com/posadeus/rover/domain/Movement.kt
+++ src/main/kotlin/com/posadeus/rover/domain/Movement.kt
@@ -1,8 +1,24 @@
 package com.posadeus.rover.domain
 
+import com.posadeus.rover.domain.Orientation.*
+import com.posadeus.rover.domain.exception.CommandNotFoundException
+
 class Movement {
 
-  fun move(coordinate: Coordinate, command: Char, orientation: Orientation): Coordinate {
-    TODO("Not yet implemented")
-  }
+  fun move(coordinate: Coordinate,
+           command: Char,
+           orientation: Orientation): Coordinate =
+      when (command) {
+        'f' -> forward(coordinate, orientation)
+        else -> throw CommandNotFoundException()
+      }
+
+  private fun forward(coordinate: Coordinate,
+                      orientation: Orientation): Coordinate =
+      when (orientation) {
+        N -> Coordinate(coordinate.x, coordinate.y + 1)
+        S -> Coordinate(coordinate.x, coordinate.y - 1)
+        E -> Coordinate(coordinate.x + 1, coordinate.y)
+        W -> Coordinate(coordinate.x - 1, coordinate.y)
+      }
 }
---
+++ src/test/kotlin/com/posadeus/rover/domain/MovementTest.kt
@@ -0,0 +1,19 @@
+package com.posadeus.rover.domain
+
+import com.posadeus.rover.domain.Orientation.*
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+internal class MovementTest {
+
+  private val movement = Movement()
+
+  @Test
+  internal fun `move forward`() {
+
+    assertTrue { movement.move(Coordinate(0, 0), 'f', N) == Coordinate(0, 1) }
+    assertTrue { movement.move(Coordinate(0, 0), 'f', E) == Coordinate(1, 0) }
+    assertTrue { movement.move(Coordinate(0, 0), 'f', S) == Coordinate(0, -1) }
+    assertTrue { movement.move(Coordinate(0, 0), 'f', W) == Coordinate(-1, 0) }
+  }
+}
