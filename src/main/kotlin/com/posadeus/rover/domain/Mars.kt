e39a21b32b64bdff1ded9c92d41babf3272c0ac4
mberti <marco.berti90@gmail.com>
2022-07-08 10:58:54 +0200

    Refactor: change coordinates reference removing negative values

--- src/main/kotlin/com/posadeus/rover/domain/Mars.kt
+++ src/main/kotlin/com/posadeus/rover/domain/Mars.kt
@@ -1,12 +1,26 @@
 package com.posadeus.rover.domain
 
-class Mars(private val x: Array<Int>,
-           private val y: Array<Int>,
-           private val obstacles: Array<Obstacle> = emptyArray()) {
+class Mars(@Deprecated("use coordinates") private val x: Array<Int>,
+           @Deprecated("use coordinates") private val y: Array<Int>,
+           private val obstacles: Array<Obstacle> = emptyArray(),
+           private val coordinates: Array<Array<Obstacle?>> = emptyArray()) {
 
+  @Deprecated("coordinates reference changed: no negative coordinates")
   fun isValidCoordinate(coordinate: Coordinate): Boolean =
       this.x.any { it == coordinate.x } && this.y.any { it == coordinate.y }
 
+  fun isValidCoordinateNew(coordinate: Coordinate): Boolean =
+      coordinate.x >= 0
+      && coordinate.y >= 0
+      && coordinates.size >= coordinate.x
+      && coordinates[coordinate.x].size >= coordinate.y
+
   fun hasObstacle(coordinate: Coordinate): Boolean =
       obstacles.any { it.isPresent(coordinate) }
+
+  fun hasObstacleNew(coordinate: Coordinate): Boolean =
+      when (coordinates[coordinate.x][coordinate.y]) {
+        is Obstacle -> true
+        else -> false
+      }
 }
--- src/test/kotlin/com/posadeus/rover/domain/MarsTest.kt
+++ src/test/kotlin/com/posadeus/rover/domain/MarsTest.kt
@@ -10,19 +10,37 @@ class MarsTest {
   internal fun `define Mars surface`() {
 
     val mars = Mars(arrayOf(-2, -1, 0, 1, 2),
-                    arrayOf(-2, -1, 0, 1, 2))
+                    arrayOf(-2, -1, 0, 1, 2),
+                    emptyArray(),
+                    arrayOf(arrayOf(null, null, null, Obstacle(Coordinate(0, 3)), null),
+                            arrayOf(null, Obstacle(Coordinate(1, 1)), null, null, null),
+                            arrayOf(null, null, null, null, null),
+                            arrayOf(null, null, null, null, Obstacle(Coordinate(3, 4))),
+                            arrayOf(null, Obstacle(Coordinate(4, 1)), Obstacle(Coordinate(4, 2)), null, null)))
 
     assertTrue { mars.isValidCoordinate(Coordinate(1, 1)) }
     assertFalse { mars.isValidCoordinate(Coordinate(-3, 1)) }
+
+    assertTrue { mars.isValidCoordinateNew(Coordinate(1, 1)) }
+    assertFalse { mars.isValidCoordinateNew(Coordinate(-3, 1)) }
   }
+
   @Test
   internal fun `Mars surface with obstacles`() {
 
     val mars = Mars(arrayOf(-2, -1, 0, 1, 2),
                     arrayOf(-2, -1, 0, 1, 2),
-                    arrayOf(Obstacle(Coordinate(-1, -2))))
+                    arrayOf(Obstacle(Coordinate(-1, -2))),
+                    arrayOf(arrayOf(null, null, null, Obstacle(Coordinate(0, 3)), null),
+                            arrayOf(null, Obstacle(Coordinate(1, 1)), null, null, null),
+                            arrayOf(null, null, null, null, null),
+                            arrayOf(null, null, null, null, Obstacle(Coordinate(3, 4))),
+                            arrayOf(null, Obstacle(Coordinate(4, 1)), Obstacle(Coordinate(4, 2)), null, null)))
 
     assertTrue { mars.hasObstacle(Coordinate(-1, -2)) }
     assertFalse { mars.hasObstacle(Coordinate(1, -2)) }
+
+    assertTrue { mars.hasObstacleNew(Coordinate(0, 3)) }
+    assertFalse { mars.hasObstacleNew(Coordinate(1, 2)) }
   }
 }
