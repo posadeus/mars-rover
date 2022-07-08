f4a8e791ec9287c24410055493803132c9f4e90c
mberti <marco.berti90@gmail.com>
2022-07-08 10:04:47 +0200

    Obstacles detection added to Obstacle

--- src/main/kotlin/com/posadeus/rover/domain/Obstacle.kt
+++ src/main/kotlin/com/posadeus/rover/domain/Obstacle.kt
@@ -1,3 +1,7 @@
 package com.posadeus.rover.domain
 
-class Obstacle(val coordinate: Coordinate)
+class Obstacle(val coordinate: Coordinate) {
+
+  fun isPresent(coordinate: Coordinate): Boolean =
+      this.coordinate == coordinate
+}
