755ee3cfa09923efc97e05bb010f2cd7973224bc
mberti <marco.berti90@gmail.com>
2022-07-08 12:40:11 +0200

    Refactor: rename

--- src/main/kotlin/com/posadeus/rover/domain/Movement.kt
+++ src/main/kotlin/com/posadeus/rover/domain/Movement.kt
@@ -20,24 +20,20 @@ class Movement {
                       orientation: Orientation): Coordinate =
       when (orientation) {
         N -> Coordinate(coordinate.x,
-                        possibleMoveOverTheEdge(isMovingInsideEdges(mars, coordinate)
-                                                { Coordinate(coordinate.x, coordinate.y + 1) },
-                                                coordinate.y + 1,
-                                                0))
+                        move(isMovingInsideEdges(mars, coordinate) { Coordinate(coordinate.x, coordinate.y + 1) },
+                             coordinate.y + 1,
+                             0))
         S -> Coordinate(coordinate.x,
-                        possibleMoveOverTheEdge(isMovingInsideEdges(mars, coordinate)
-                                                { Coordinate(coordinate.x, coordinate.y - 1) },
-                                                coordinate.y - 1,
-                                                4))
-        E -> Coordinate(possibleMoveOverTheEdge(isMovingInsideEdges(mars, coordinate)
-                                                { Coordinate(coordinate.x + 1, coordinate.y) },
-                                                coordinate.x + 1,
-                                                0),
+                        move(isMovingInsideEdges(mars, coordinate) { Coordinate(coordinate.x, coordinate.y - 1) },
+                             coordinate.y - 1,
+                             4))
+        E -> Coordinate(move(isMovingInsideEdges(mars, coordinate) { Coordinate(coordinate.x + 1, coordinate.y) },
+                             coordinate.x + 1,
+                             0),
                         coordinate.y)
-        W -> Coordinate(possibleMoveOverTheEdge(isMovingInsideEdges(mars, coordinate)
-                                                { Coordinate(coordinate.x - 1, coordinate.y) },
-                                                coordinate.x - 1,
-                                                4),
+        W -> Coordinate(move(isMovingInsideEdges(mars, coordinate) { Coordinate(coordinate.x - 1, coordinate.y) },
+                             coordinate.x - 1,
+                             4),
                         coordinate.y)
       }
 
@@ -51,9 +47,9 @@ class Movement {
         W -> forward(mars, coordinate, E)
       }
 
-  private fun possibleMoveOverTheEdge(isMoveInsideEdges: Boolean,
-                                      insidePoint: Int,
-                                      pacmanPoint: Int) =
+  private fun move(isMoveInsideEdges: Boolean,
+                   insidePoint: Int,
+                   pacmanPoint: Int) =
       if (isMoveInsideEdges) insidePoint
       else pacmanPoint
 
