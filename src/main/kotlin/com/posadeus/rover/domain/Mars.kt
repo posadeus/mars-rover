5591d1675d5f41d0e4646058ebef7c1b3ce5acf6
mberti <marco.berti90@gmail.com>
2022-07-08 09:42:53 +0200

    Refactoring: deprecated method on Mars removed

--- src/main/kotlin/com/posadeus/rover/domain/Mars.kt
+++ src/main/kotlin/com/posadeus/rover/domain/Mars.kt
@@ -3,10 +3,6 @@ package com.posadeus.rover.domain
 class Mars(private val x: Array<Int>,
            private val y: Array<Int>) {
 
-  @Deprecated("no more needed")
-  fun coordinate(x: Int, y: Int): Coordinate =
-      Coordinate(this.x[x], this.y[y])
-
   fun isValidCoordinate(coordinate: Coordinate): Boolean =
       this.x.any { it == coordinate.x } && this.y.any { it == coordinate.y }
 }
