6b699713064da321974049843675f083f8593395
mberti <marco.berti90@gmail.com>
2022-07-29 11:30:53 +0200

    Logging errors while happening

--- src/main/kotlin/com/posadeus/rover/domain/Error.kt
+++ src/main/kotlin/com/posadeus/rover/domain/Error.kt
@@ -1,7 +1,30 @@
 package com.posadeus.rover.domain
 
-sealed class Error
+import java.util.logging.Logger
 
-data class CommandNotFound(val message: String) : Error()
-data class MissionAborted(val message: String) : Error()
-data class WrongCoordinate(val message: String) : Error()
+sealed class Error(message: String) {
+
+  init {
+
+    logger.warning(message)
+  }
+
+  companion object {
+
+    private val logger = Logger.getLogger("Error")
+  }
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (javaClass != other?.javaClass) return false
+    return true
+  }
+
+  override fun hashCode(): Int {
+    return javaClass.hashCode()
+  }
+}
+
+class CommandNotFound(message: String) : Error(message)
+class MissionAborted(message: String) : Error(message)
+class WrongCoordinate(message: String) : Error(message)
